e6836caba16b20afe367407f66a5b9b764634668
Ömer Hancı <hanciomer@gmail.com>
2015-11-25 17:33:26 +0200

    UI Assets are added
    Assest are added for main menu panels.

---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+    GameObject optionsPanel;
+    GameObject highScoresPanel;
+    GameObject selectDifficultyPanel;
+    
+    void Start()
+    {
+        hidePassiveMenus();
+        optionsPanel.SetActive(false);
+        highScoresPanel.SetActive(false);
+        selectDifficultyPanel.SetActive(false);
+    }
+
+    public void hidePassiveMenus()
+    {
+        optionsPanel = GameObject.Find("OptionsPanel");
+        highScoresPanel = GameObject.Find("HighScoresPanel");
+        selectDifficultyPanel = GameObject.Find("SelectDifficultyPanel");
+    }
+
+	public void LoadScene(int level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    public void OpenChildMenu(string panel)
+    {
+        if (panel.Equals("OptionsPanel"))
+        {
+            optionsPanel.SetActive(true);
+        }
+        else if (panel.Equals("HighScoresPanel"))
+        {
+            highScoresPanel.SetActive(true);
+        }
+        else if (panel.Equals("SelectDifficultyPanel"))
+        {
+            selectDifficultyPanel.SetActive(true);
+        }
+
+    }
+    public void BackToMainMenu(string panel)
+    {
+        if (panel.Equals("OptionsPanel"))
+        {
+            optionsPanel.SetActive(false);
+        }
+        else if (panel.Equals("HighScoresPanel"))
+        {
+            highScoresPanel.SetActive(false);
+        }
+        else if (panel.Equals("SelectDifficultyPanel"))
+        {
+            selectDifficultyPanel.SetActive(false);
+        }
+    }
+  
+}
